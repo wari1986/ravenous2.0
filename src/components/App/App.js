561ba3c5b7634df24d3c06863c9e873985dbff8e
wari.1986 <nicolaycamacho@gmail.com>
2020-06-17 23:19:04 -0400

    Completed App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BusinessList from '../components/BusinessList';
+import SearchBar from '../components/SearchBar';
+
+
 
 function App() {
   return (
