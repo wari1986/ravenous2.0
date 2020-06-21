c2c4f00e2fd045b7f0943ee400c092087cc70424
wari.1986 <nicolaycamacho@gmail.com>
2020-06-21 18:44:37 -0400

    Cut Bussines class component and pasted in App.js and created Businnes object in App.jss

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,16 +4,72 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
+class Business extends React.Component {
+
+  render() {
+    return ( <
+      div className = "Business" >
+      <
+      div className = "image-container" >
+      <
+      img src = 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg'
+      alt = '' / >
+      <
+      /div> <
+      h2 > {
+        business.name
+      } < /h2> <
+      div className = "Business-information" >
+      <
+      div className = "Business-address" >
+      <
+      p > {
+        business.address
+      } < /p> <
+      p > {
+        business.city
+      } < /p> <
+      p > {
+        business.state
+      } {
+        business.zipCode
+      } < /p> < /
+      div > <
+      div className = "Business-reviews" >
+      <
+      h3 > {
+        business.category
+      } < /h3> <
+      h3 className = "rating" > {
+        business.rating
+      }
+      stars
+    } < /h3> <
+    p > {
+      business.reviewCount
+    } < /p> < /
+    div > <
+      /div> < /
+    div >
+  );
+}
+}
+
 class App extends React.Component {
-  render(){
-    return (
-      <div className="App">
-        <h1>Ravenous</h1>
-        <SearchBar />
-        <BusinessList />
-      </div>
+  render() {
+    return ( <
+      div className = "App" >
+      <
+      h1 > Ravenous < /h1> <
+      SearchBar / >
+      <
+      BusinessList / >
+      <
+      /div>
     );
   }
 }
+
+const Business = [business, business, business, business, business, business];
 
 export default App;
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,30 +9,41 @@ let sortByOptions = {
 
 class SearchBar extends React.Component {
 
-  renderSortByOptions(){
+  renderSortByOptions() {
     return Object.keys(sortByOptions).map(sortByOption => {
-    let sortByOptionValue=sortByOptions[sortByOption];
-    return <li key={sortByOptionValue}>{sortByOption}</li>;
-  });
-}
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key = {
+        sortByOptionValue
+      } > {
+        sortByOption
+      } < /li>;
+    });
+  }
 
   render() {
-    return (
-    <div className="SearchBar">
-      <div className="SearchBar-sort-options">
-        <ul>
-          {this.renderSortByOptions()}
-        </ul>
-      </div>
-      <div className="SearchBar-fields">
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
-      </div>
-      <div className="SearchBar-submit">
-        <a>Let's Go</a>
-      </div>
-    </div>
-  );
+    return ( <
+      div className = "SearchBar" >
+      <
+      div className = "SearchBar-sort-options" >
+      <
+      ul > {
+        this.renderSortByOptions()
+      } <
+      /ul> < /
+      div > <
+      div className = "SearchBar-fields" >
+      <
+      input placeholder = "Search Businesses" / >
+      <
+      input placeholder = "Where?" / >
+      <
+      /div> <
+      div className = "SearchBar-submit" >
+      <
+      a > Let 's Go</a> < /
+      div > <
+      /div>
+    );
   };
 }
 
