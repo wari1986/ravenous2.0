f6a4a20847862b7c75fe375aa764081196778272
wari.1986 <nicolaycamacho@gmail.com>
2020-06-21 19:14:08 -0400

    this.props added to Business component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,72 +4,42 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-class Business extends React.Component {
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Bordertown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+};
 
-  render() {
-    return ( <
-      div className = "Business" >
-      <
-      div className = "image-container" >
-      <
-      img src = 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg'
-      alt = '' / >
-      <
-      /div> <
-      h2 > {
-        business.name
-      } < /h2> <
-      div className = "Business-information" >
-      <
-      div className = "Business-address" >
-      <
-      p > {
-        business.address
-      } < /p> <
-      p > {
-        business.city
-      } < /p> <
-      p > {
-        business.state
-      } {
-        business.zipCode
-      } < /p> < /
-      div > <
-      div className = "Business-reviews" >
-      <
-      h3 > {
-        business.category
-      } < /h3> <
-      h3 className = "rating" > {
-        business.rating
-      }
-      stars
-    } < /h3> <
-    p > {
-      business.reviewCount
-    } < /p> < /
-    div > <
-      /div> < /
-    div >
-  );
-}
-}
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+];
 
 class App extends React.Component {
   render() {
     return ( <
       div className = "App" >
       <
-      h1 > Ravenous < /h1> <
+      h1 > ravenous < /h1> <
       SearchBar / >
       <
-      BusinessList / >
-      <
+      BusinessList businesses = {
+        businesses
+      }
+      /> <
       /div>
     );
   }
 }
-
-const Business = [business, business, business, business, business, business];
 
 export default App;
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -4,15 +4,16 @@ import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render() {
-    return(
-      <div className="BusinessList">
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-        <Business />
-      </div>
+    return ( <
+      div className = "BusinessList" > {
+        this.props.businesses.map(business => {
+          return <Business business = {
+            business
+          };
+          />
+        });
+      } <
+      /div>
     )
   }
 }
