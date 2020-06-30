498b285d22fac6e2fd0872a4784f5b3602dadca5
wari.1986 <nicolaycamacho@gmail.com>
2020-06-29 21:07:42 -0400

    Added Yelp API

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,40 +4,32 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList';
 import SearchBar from '../SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Bordertown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-];
+import Yelp from '../../util/Yelp';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      businesses: []
+    };
+
+    this.searchYelp = this.searchYelp.bind(this);
+  }
+
+  searchYelp(term, location, sortBy) {
+    Yelp.searchYelp(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses});
+    });
+  }
+
   render() {
-    return ( <
-      div className = "App" >
-      <
-      h1 > ravenous < /h1> <
-      SearchBar / >
-      <
-      BusinessList businesses = {
-        businesses
-      }
-      /> <
-      /div>
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} />
+      </div>
     );
   }
 }
--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './BusinessList.css';
+
 import Business from '../Business/Business';
 
 class BusinessList extends React.Component {
   render() {
-    return ( <
-      div className = "BusinessList" > {
-        this.props.businesses.map(business => {
-          return <Business business = {
-            business
-          };
-          />
-        });
-      } <
-      /div>
-    )
+    return (
+      <div className="BusinessList">
+        {
+          this.props.businesses.map(business => {
+            return <Business business={business} key={business.id} />
+          })
+        }
+      </div>
+    );
   }
 }
 
